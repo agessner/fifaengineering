cc7f5ecf9578afe92cfe1133b30b0bc58196a710
Airton Jeferson Gessner <airton@mercos.com>
2020-06-10 22:59:55 -0300

    creating pipeline for storing the output in google bigquery

--- scrapy_sofifa/scrapy_sofifa/pipelines.py
+++ scrapy_sofifa/scrapy_sofifa/pipelines.py
@@ -4,8 +4,48 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from datetime import datetime
+
+from google.cloud import bigquery
+from google.cloud.bigquery import Dataset, Table, LoadJobConfig
 
 
 class ScrapySofifaPipeline:
+    def __init__(self):
+        self.bigquery_connection = None
+        self.table = None
+        self.current_datetime = datetime.now()
+
     def process_item(self, item, spider):
-        return item
+        return {
+            'value': item['url'],
+            'processed_at': self.current_datetime
+        }
+
+    def open_spider(self, spider):
+        self.bigquery_connection = bigquery.Client(project='fifaengineering')
+        self.bigquery_connection.create_dataset(
+            Dataset('fifaengineering.sofifa'),
+            exists_ok=True
+        )
+        self.table = Table('fifaengineering.sofifa.urls', schema=[
+            bigquery.schema.SchemaField('value', 'STRING'),
+            bigquery.schema.SchemaField('processed_at', 'DATETIME')
+        ])
+        self.bigquery_connection.create_table(self.table, exists_ok=True)
+
+    def close_spider(self, spider):
+        job_config = LoadJobConfig(
+            source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+            write_disposition=bigquery.WriteDisposition.WRITE_APPEND
+        )
+        filename = spider.settings.get('PLAYERS_URL_LIST_URI')
+        with open(filename, "rb") as source_file:
+            job = self.bigquery_connection.load_table_from_file(
+                source_file,
+                self.table,
+                job_config=job_config
+            )
+
+        job.result()
+        self.bigquery_connection.close()
--- scrapy_sofifa/scrapy_sofifa/settings.py
+++ scrapy_sofifa/scrapy_sofifa/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scrapy_sofifa.pipelines.ScrapySofifaPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'scrapy_sofifa.pipelines.ScrapySofifaPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +88,13 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+PLAYERS_URL_LIST_URI = 'data/urls.jl'
+FEEDS = {
+   PLAYERS_URL_LIST_URI: {
+      'format': 'jsonlines',
+      'store_empty': True,
+      'encoding': 'utf8',
+      'fields': None,
+      'indent': 4
+   }
+}
