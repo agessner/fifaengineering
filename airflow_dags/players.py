78dd6f837c16e5ebb49c40ed13cfe807e85ced59
Airton Jeferson Gessner <airton@mercos.com>
2020-06-21 14:02:38 -0300

    adding players pipeline to airflow

--- airflow_dags/players.py
+++ airflow_dags/players.py
@@ -70,5 +70,92 @@ urls_list_to_bigquery = GoogleCloudStorageToBigQueryOperator(
     dag=dag
 )
 
+players_task = BashOperator(
+    task_id='players',
+    bash_command='cd {scrapy_path} && scrapy crawl players'.format(scrapy_path=SCRAPY_PATH),
+    dag=dag,
+    depends_on_past=True,
+    wait_for_downstream=True,
+)
 
-generate_current_date >> get_urls_task >> urls_list_to_gcs >> urls_list_to_bigquery
+
+gcs_players_path = '{current_date}/players-{current_date_time}.jl'.format(
+    current_date=date.strftime(date.today(), '%Y-%m-%d'),
+    current_date_time="{{ task_instance.xcom_pull(task_ids='generate_current_date') }}"
+)
+
+players_list_to_gcs = FileToGoogleCloudStorageOperator(
+    task_id='players_list_to_gcs',
+    src='{scrapy_path}/data/players.jl'.format(scrapy_path=SCRAPY_PATH),
+    dst=gcs_players_path,
+    google_cloud_storage_conn_id='google_cloud_default',
+    bucket='sofifa',
+    depends_on_past=True,
+    wait_for_downstream=True,
+    mime_type='application/x-ndjson',
+    dag=dag
+)
+
+players_list_to_bigquery = GoogleCloudStorageToBigQueryOperator(
+    task_id='players_list_to_bq',
+    bucket='sofifa',
+    source_objects=[gcs_path],
+    destination_project_dataset_table='fifaengineering.sofifa.players',
+    schema_fields=[
+        {'name': 'id', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'full_name', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'image_url', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'country', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'country_image_url', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'positions', 'type': 'STRING', 'mode': 'REPEATED'},
+        {'name': 'age', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'birthdate', 'type': 'DATE', 'mode': 'NULLABLE'},
+        {'name': 'height_in_meters', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'weight_in_kg', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'overall_rating', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'potential_rating', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'value_in_million_euros', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'wage_in_thousand_euros', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'preferred_foot', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'weak_foot', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'skill_moves', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'international_reputation', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'work_rate', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'body_type', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'real_face', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'release_clause', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'specialities', 'type': 'STRUCT', 'mode': 'REPEATED', 'fields': (
+            {'name': 'id', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+            {'name': 'link', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'text', 'type': 'STRING', 'mode': 'NULLABLE'},
+        )},
+        {'name': 'team_name', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'team_url', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'team_image_url', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'team_overall', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'team_position', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'team_jersey_number', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'joined', 'type': 'DATE', 'mode': 'NULLABLE'},
+        {'name': 'loaned_from', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'loaned_from_team_url', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'contract_valid_until', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'national_team_name', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'national_team_url', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'national_team_image_url', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'national_team_overall', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'national_team_position', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'national_team_jersey_number', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'processed_at', 'type': 'DATETIME', 'mode': 'NULLABLE'},
+        {'name': 'value', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'processed_at', 'type': 'DATETIME', 'mode': 'NULLABLE'}
+    ],
+    source_format='NEWLINE_DELIMITED_JSON',
+    bigquery_conn_id='google_cloud_default',
+    write_disposition='WRITE_APPEND',
+    depends_on_past=True,
+    wait_for_downstream=True,
+    dag=dag
+)
+
+generate_current_date >> get_urls_task >> urls_list_to_gcs >> urls_list_to_bigquery >> \
+    players_task >> gcs_players_path >> players_list_to_gcs >> players_list_to_bigquery
