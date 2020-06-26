8928ca6fc0b362e85919db53c2ff7f065c398e05
Airton Jeferson Gessner <airton@mercos.com>
2020-06-26 20:29:28 -0300

    changing node configs

--- composer_scripts/create_environment.sh
+++ composer_scripts/create_environment.sh
@@ -7,6 +7,8 @@ gcloud composer environments create fifaeng \
     --python-version 3 \
     --image-version composer-latest-airflow-1.10.2 \
     --machine-type n1-standard-4 \
+    --airflow-configs CORE-WORKER_CONCURRENCY 10 \
+    --node-count 4 \
     --service-account dataloader@fifaeng.iam.gserviceaccount.com &&
 gcloud composer environments update fifaeng \
     --update-pypi-packages-from-file requirements_composer.txt \
