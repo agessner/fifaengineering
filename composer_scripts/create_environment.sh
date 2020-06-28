15c7dd8b36a83cc22179d3f71a5785d1a30821d7
Airton Jeferson Gessner <airton@mercos.com>
2020-06-28 10:44:24 -0300

    fixing configs

--- composer_scripts/create_environment.sh
+++ composer_scripts/create_environment.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-gcloud composer environments create fifaeng2 \
+gcloud composer environments create fifaeng \
     --location us-central1 \
+    --zone us-central1-f \
+    --machine-type n1-standard-2 \
     --disk-size 20 \
     --env-variables SCRAPY_PATH=/home/airflow/gcs/dags/dependencies/scrapy_sofifa/ \
     --python-version 3 \
     --image-version composer-latest-airflow-1.10.2 \
-    --machine-type n1-standard-4 \
-    --zone us-central1-d \
     --airflow-configs core-worker_concurrency=10 \
     --node-count 4 \
     --service-account dataloader@fifaeng.iam.gserviceaccount.com &&
-gcloud composer environments update fifaeng2 \
+gcloud composer environments update fifaeng \
     --update-pypi-packages-from-file requirements_composer.txt \
     --location us-central1
