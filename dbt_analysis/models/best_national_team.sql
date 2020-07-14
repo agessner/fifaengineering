0e0cd8c96395cd3a2b4abd7df2a1633123b4dd93
Fifa Engineering <fifaengineering@gmail.com>
2020-07-14 14:26:04 +0000

    returning second best player at each position

--- dbt_analysis/models/best_national_team.sql
+++ dbt_analysis/models/best_national_team.sql
@@ -6,6 +6,17 @@ WITH
       MAX(overall_rating) AS rating
     FROM {{ref('players')}} 
     GROUP BY country, player_position
+  ),
+  second_max_rating_by_position AS (
+    SELECT 
+      second_max_rating.country,
+      second_max_rating.player_position, 
+      ARRAY_AGG(second_max_rating.overall_rating ORDER BY second_max_rating.overall_rating DESC LIMIT 1)[SAFE_OFFSET(0)] AS rating
+    FROM {{ref('players')}} second_max_rating
+    JOIN max_rating_by_position ON max_rating_by_position.country = second_max_rating.country
+    AND max_rating_by_position.player_position = second_max_rating.player_position 
+    AND max_rating_by_position.rating >= second_max_rating.overall_rating
+    GROUP BY country, player_position    
   )
 
 SELECT
@@ -15,3 +26,7 @@ JOIN max_rating_by_position
 ON max_rating_by_position.player_position = players.player_position
 AND max_rating_by_position.country = players.country
 AND max_rating_by_position.rating = players.overall_rating
+JOIN second_max_rating_by_position 
+ON second_max_rating_by_position.player_position = players.player_position
+AND second_max_rating_by_position.country = players.country
+AND second_max_rating_by_position.rating = players.overall_rating
