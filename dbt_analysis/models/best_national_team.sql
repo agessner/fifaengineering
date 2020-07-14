61fe30eb3788bb1614b58b33b8c307d63a699ca4
Airton Gessner <ajgessner@hotmail.com>
2020-07-14 12:06:57 -0300

    fix

--- dbt_analysis/models/best_national_team.sql
+++ dbt_analysis/models/best_national_team.sql
@@ -7,25 +7,32 @@ WITH
     FROM {{ref('players')}} 
     GROUP BY country, player_position
   ),
+  max_rating_player AS (
+    SELECT
+      players.*
+    FROM {{ref('players')}} players
+    JOIN max_rating_by_position 
+    ON max_rating_by_position.player_position = players.player_position
+    AND max_rating_by_position.country = players.country
+    AND max_rating_by_position.rating = players.overall_rating
+  ),
   second_max_rating_by_position AS (
     SELECT 
       second_max_rating.country,
       second_max_rating.player_position, 
-      ARRAY_AGG(max_rating_by_position.rating ORDER BY max_rating_by_position.rating DESC LIMIT 2)[SAFE_OFFSET(1)] AS rating
+      ARRAY_AGG(second_max_rating.overall_rating ORDER BY second_max_rating.overall_rating DESC LIMIT 1)[SAFE_OFFSET(0)] AS rating
     FROM {{ref('players')}} second_max_rating
-    JOIN max_rating_by_position ON max_rating_by_position.country = second_max_rating.country
-    AND max_rating_by_position.player_position = second_max_rating.player_position 
+    JOIN max_rating_player ON max_rating_player.country = second_max_rating.country
+    AND max_rating_player.player_position = second_max_rating.player_position
+    AND max_rating_player.id != second_max_rating.id
+    AND max_rating_player.overall_rating >= second_max_rating.overall_rating 
     GROUP BY country, player_position    
   )
 
 SELECT
   players.* EXCEPT(position_order),
   positions.position_order  
-FROM {{ref('players')}} players
-JOIN max_rating_by_position 
-ON max_rating_by_position.player_position = players.player_position
-AND max_rating_by_position.country = players.country
-AND max_rating_by_position.rating = players.overall_rating
+FROM max_rating_player players
 LEFT JOIN {{ref('positions')}} positions ON positions.position = players.player_position
 WHERE players.country = 'England'
 UNION ALL
