3f623c05e47222aaa909fa1267bb3864f525323c
Fifa Engineering <fifaengineering@gmail.com>
2020-07-14 11:14:59 +0000

    using team_position

--- dbt_analysis/models/worst_team.sql
+++ dbt_analysis/models/worst_team.sql
@@ -2,17 +2,17 @@ WITH
   min_rating_by_position AS (
     SELECT 
       team_name,
-      player_position, 
+      team_position, 
       MIN(overall_rating) AS rating
     FROM {{ref('unique_players_by_team_position')}} 
-    GROUP BY team_name, player_position
+    GROUP BY team_name, team_position
   )
 
 SELECT
   players.* 
 FROM {{ref('unique_players_by_team_position')}} players
 JOIN min_rating_by_position 
-ON min_rating_by_position.player_position = players.player_position
+ON min_rating_by_position.team_position = players.team_position
 AND min_rating_by_position.team_name = players.team_name
 AND min_rating_by_position.rating = players.overall_rating
-WHERE players.player_position != 'RES' AND players.player_position != 'SUB'
+WHERE players.team_position != 'RES' AND players.team_position != 'SUB'
