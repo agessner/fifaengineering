c155986709b6a5962c89ade98a5ed23cb738f907
Airton Gessner <ajgessner@hotmail.com>
2020-07-15 12:38:39 -0300

    Update best_team_potential.sql

--- dbt_analysis/models/best_team_potential.sql
+++ dbt_analysis/models/best_team_potential.sql
@@ -1,18 +1,31 @@
 WITH
+  players AS (
+      SELECT
+          players.id,
+          players.name,
+          players.age,
+          players.team_name,
+          players.player_position,
+          players.potential_overall_rating AS rating,
+          players.version_name,
+          players.image_url,
+          players.team_image_url
+      FROM {{ref('players')}} players
+  ),
   max_rating_by_position AS (
     SELECT 
       team_name,
-      player_position, 
-      MAX(potential_overall_rating) AS rating
-    FROM {{ref('unique_players_by_team_position')}} 
-    GROUP BY team_name, player_position
+      player_position,
+      version_name,
+      MAX(rating) AS rating
+    FROM players 
+    GROUP BY team_name, player_position, version_name
   )
 
 SELECT
   players.* 
-FROM {{ref('unique_players_by_team_position')}} players
-JOIN max_rating_by_position 
+FROM players JOIN max_rating_by_position
 ON max_rating_by_position.player_position = players.player_position
 AND max_rating_by_position.team_name = players.team_name
-AND max_rating_by_position.rating = players.overall_rating
-WHERE players.player_position != 'RES' AND players.player_position != 'SUB'
+AND max_rating_by_position.rating = players.rating
+AND max_rating_by_position.version_name = players.version_name
